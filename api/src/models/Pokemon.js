0b107e899fc934fea820197996b689d0e2464afe
[leamercado] <[leandromercado38@gmail.com]>
2022-08-12 14:03:28 -0300

    DB

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -1,12 +1,49 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, UUIDV4 } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('pokemon', {
+  sequelize.define("pokemon", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    hp: {   // vida
+      type: DataTypes.STRING,
+    },
+    attack: {
+      type: DataTypes.STRING,
+    },
+    defense: {
+      type: DataTypes.STRING,
+    },
+    speed: {
+      type: DataTypes.STRING,
+    },
+    height: {
+      type: DataTypes.STRING,
+    },
+    weight: {
+      type: DataTypes.STRING,
+    },
+    image:{  // no obligatorio
+      type: DataTypes.STRING
+
+    },
+    createdInDb: {
+      type:DataTypes.BOOLEAN,
+      // allowNull:false,
+      defaultValue: false,
+    }
+  }, 
+  {timestamps: false});
 };
+
+// tabla con los datos del pokemon, ésto lo relaciono con la tabla Type (donde voy a guardar todos los tipos traidos desde la API). 
+// A cada Pokemon creado le voy a poder asignarle tipo/s
